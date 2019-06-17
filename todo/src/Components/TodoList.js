b78274b50e371bf0e9a3a1af7d2b51ae05d220a6
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-17 14:39:36 +0200

    Feature: added completed functionality

--- todo/src/Components/TodoList.js
+++ todo/src/Components/TodoList.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux'; 
+import { connect } from 'react-redux';
+import '../App.css'
 
 const TodoList = (props) => {
         return ( 
                 <ul>
-                        {props.todos.map(todo => <li>{todo.todo}</li>)}
+                        {props.todos.map(todo => <li 
+                        className={todo.completed === false ? null : 'line-through'}
+                        onClick={() => props.changeToCompleted(todo.id)}>
+                        {todo.todo}
+                        </li>)}
                 </ul>
          );
 }
@@ -13,6 +18,12 @@ const mapStateToProps = (state) => {
         return {
                 todos: state.todos
         }
+};
+
+const mapDispatchToProps = dispatch => {
+        return {
+                changeToCompleted: (id) => dispatch({type: 'CHANGE_TO_COMPLETED', id: id})
+        }
 }
 
-export default connect(mapStateToProps)(TodoList);
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- todo/src/store/reducer.js
+++ todo/src/store/reducer.js
@@ -9,6 +9,13 @@ export default (state = initialState, action) => {
                 case ADD_TODO:
                         const newTodo = [...state.todos, action.payload]
                         return Object.assign({},state , {todos: newTodo});
+                case 'CHANGE_TO_COMPLETED':
+                        const updateTodos = state.todos.map(todo => {
+                                if(todo.id === action.id){
+                                        todo.completed = true
+                                } return todo
+                        });
+                        return Object.assign({}, state, {todos: updateTodos})
                         default: return state;
         }
 }
