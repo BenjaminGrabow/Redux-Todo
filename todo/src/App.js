9114711d129c6ced3d62892daee935d3877e0e99
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-17 14:57:56 +0200

    Feature: added delete functionality

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import TodoInput from './Components/TodoInput';
 import TodoList from './Components/TodoList';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <TodoList />
+      <button onClick={props.deleteItems} >Delete</button>
       <TodoInput />
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = state => {
+  return {}
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteItems: () => dispatch({ type: 'DELETE_ITEMS'})
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- todo/src/store/reducer.js
+++ todo/src/store/reducer.js
@@ -7,15 +7,20 @@ const initialState = {
 export default (state = initialState, action) => {
         switch(action.type){
                 case ADD_TODO:
-                        const newTodo = [...state.todos, action.payload]
+                        const newTodo = [...state.todos, action.payload];
                         return Object.assign({},state , {todos: newTodo});
-                case 'CHANGE_TO_COMPLETED':
+                
+                        case 'CHANGE_TO_COMPLETED':
                         const updateTodos = state.todos.map(todo => {
                                 if(todo.id === action.id){
                                         todo.completed = true
                                 } return todo
                         });
-                        return Object.assign({}, state, {todos: updateTodos})
+                        return Object.assign({}, state, {todos: updateTodos});
+
+                        case 'DELETE_ITEMS':
+                          const updateTodoList = state.todos.filter(todo => !todo.completed);
+                          return Object.assign({}, state, {todos: updateTodoList})
                         default: return state;
         }
 }
